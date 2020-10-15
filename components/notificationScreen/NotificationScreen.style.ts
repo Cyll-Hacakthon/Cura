c3b73cabb1f9a4a2691b1ac2b2e44a68c46816ca
Richard Lim <richmars.lim@gmail.com>
2020-10-15 14:13:20 +0800

    Add No Notification Indicator in NotificationScreen

---
+++ components/notificationScreen/NotificationScreen.style.ts
@@ -0,0 +1,13 @@
+import {StyleSheet} from 'react-native';
+
+const NotificationScreenStyle = StyleSheet.create({
+  container: {
+    paddingTop: 55,
+  },
+  noNotificationIndicator: {
+    textAlign: 'center',
+    color: 'grey',
+  },
+});
+
+export default NotificationScreenStyle;
--- components/notificationScreen/NotificationScreen.tsx
+++ components/notificationScreen/NotificationScreen.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import Style from './NotificationScreen.style';
+import {Feather} from '@expo/vector-icons';
 
 const NotificationScreen = () => {
   return (
-    <View>
-      <Text>This is notification page</Text>
+    <View style={Style.container}>
+      <Text style={Style.noNotificationIndicator}>
+        <Feather name="smile" color="grey" size={15} />
+        {'  '}
+        You Have No Notifications To See.
+      </Text>
     </View>
   );
 };
